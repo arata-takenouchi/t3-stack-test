3723a3ba4c6576dceaff87dc278e965f930f40fa
a-takenouchi_ddorg <a.takenouchi@dentsudigital.co.jp>
2024-12-12 23:45:00 +0900

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link";
 import { db } from "~/server/db"
+
+export const dynamic = "force-dynamic"
 
 const mockUrls = [
   "https://utfs.io/f/PRzMhZV0Z7s9T80ktzbGhPQHwkNvrI6W91cUejqxy4dBKmAa",
